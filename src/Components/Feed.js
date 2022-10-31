d6038ebf612db5679e2891c9df622d6bcffd8243
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-31 18:20:53 +0530

    Created sub-components for Navbar

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,33 +1,38 @@
 import React, { useEffect, useContext, useState } from 'react'
 import context from '../Context'
 import { database } from './Firebase';
+import Navbar from './Navbar';
 import Posts from './Posts';
 import UploadFile from './UploadFile'
 
 
 function Feed() {
-  const {user} = useContext(context);
+  const { user } = useContext(context);
   const [userData, setUserData] = useState('');
 
-  useEffect(()=>{
-      const unsub = database.users.doc(user.uid).onSnapshot((snapshot)=>{
-          setUserData(snapshot.data());
-      });
+  useEffect(() => {
+    const unsub = database.users.doc(user.uid).onSnapshot((snapshot) => {
+      setUserData(snapshot.data());
+    });
 
-      return ()=>{
-        unsub();
-      }
+    return () => {
+      unsub();
+    }
 
-  },[user])
+  }, [user])
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-      <div className='comp' style={{ width: '50%' }}>
+    <>
+      <Navbar userData = {userData}/>
+      <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+        {/* <div className='comp' style={{ width: '50%' }}>
         <h1>Welcome to feed</h1>
+      </div> */}
+
+        <UploadFile user={userData} />
+        <Posts userData={userData} />
       </div>
-      <UploadFile user = {userData}/>
-      <Posts userData = {userData}/>
-    </div>
+    </>
 
   )
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,173 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import { useNavigate } from 'react-router-dom';
+import context from '../Context';
+import { useContext } from 'react';
+import { makeStyles } from '@mui/styles';
+import insta from '../Assets/Instagram.jpg'
+
+const useStyles = makeStyles({
+  appbar: {
+    backgroundColor: 'white'
+  }
+})
+
+export default function Navbar({ userData }) {
+  console.log(userData);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const navigate = useNavigate();
+
+  const { logout } = useContext(context);
+
+  const classes = useStyles();
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const profilePage = () => {
+    navigate(`/profile/${userData.userId}`);
+  }
+
+  const bannerClick = () => {
+    navigate('/');
+  }
+
+  const logoutUser = async () => {
+    await logout();
+  }
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={profilePage}><AccountCircleIcon></AccountCircleIcon><p>&nbsp; &nbsp;</p>Profile</MenuItem>
+      <MenuItem onClick={logoutUser}><ExitToAppIcon></ExitToAppIcon><p>&nbsp; &nbsp;</p>Logout</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem onClick={profilePage}><AccountCircleIcon></AccountCircleIcon><p>&nbsp; &nbsp;</p>Profile</MenuItem>
+      <MenuItem onClick={logoutUser}><ExitToAppIcon></ExitToAppIcon><p>&nbsp; &nbsp;</p>Logout</MenuItem>
+    </Menu>
+  );
+  // className={classes.appbar}*/
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" className={classes}  >    {/*  yeh bhi kr skte hai style={{backgroundColor : 'white'}} */}
+        <Toolbar>
+          <div>
+
+              <img src={insta} onClick = {bannerClick} alt="" />
+          </div>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
